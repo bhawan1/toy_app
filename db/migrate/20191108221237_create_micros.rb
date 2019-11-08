3b060ecec93f1428d728fbe2f89a7e41f7fb0d9d
Bhawana Mishra <mishra.167@buckeyemail.osu.edu>
2019-11-08 22:34:01 +0000

    Finish toy app

---
+++ db/migrate/20191108221237_create_micros.rb
@@ -0,0 +1,10 @@
+class CreateMicros < ActiveRecord::Migration[6.0]
+  def change
+    create_table :micros do |t|
+      t.text :content
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
